d7a61cb47492ce11d7c66d6af76722a98e739264
cosimo.casini <cosimo.casini@gucci.com>
2026-07-16 16:30:05 +0200

    chore(key): removed comments for keys.nix

--- modules/keys.nix
+++ modules/keys.nix
@@ -1,12 +1,3 @@
-# modules/keys.nix
-#
-# Chiavi SSH autorizzate per cosimo.
-# Usato da:
-#   - modules/common.nix  (utente cosimo sul sistema installato)
-#   - hosts/installer/    (root sull'ISO installer)
-#
-# Per aggiornare la chiave: modifica solo questo file.
-
 [
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRFFsFLz9/OeeBA6HLgCvVxjbUgUERbMNaj7vAzRx7p casini.cosimo@gmail.com"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRFFsFLz9/OeeBA6HLgCvVxjbUgUERbMNaj7vAzRx7p"
 ]
